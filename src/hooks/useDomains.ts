52002058de58b1ebefae4f25a3a110c44f72367e
anastaxgr <alexanastagr@gmail.com>
2024-06-24 00:27:12 +0300

    fix return null stored domains

--- src/hooks/useDomains.ts
+++ src/hooks/useDomains.ts
@@ -3,39 +3,34 @@ import { useState, useEffect } from 'react';
 import { IDomain } from '../interfaces';
 
 const useDomains = () => {
-    const [domains, setDomains] = useState<IDomain[]>([])
+  const [domains, setDomains] = useState<IDomain[]>([]);
 
-    interface savedDomains {
-        domain:string;
-        secret: string;
-    }
+  useEffect(() => {
+    // Read domains from local storage
+    const storedDomains = localStorage.getItem('domains');
+    const parsedDomains = storedDomains ? JSON.parse(storedDomains) : [];
+    setDomains(parsedDomains);
+  }, []);
 
-    useEffect(() => {
-        const storedDomains = JSON.parse(localStorage.getItem('domains'));
-        setDomains(storedDomains);
-    }, []);
+  // insert new domain to list
+  const addDomain = (domain : IDomain) => {
+    const updatedDomains = [...domains, domain];
+    setDomains(updatedDomains);
+    localStorage.setItem('domains', JSON.stringify(updatedDomains));
+  };
 
+   // delete a saved domain
+  const removeDomain = (domain : IDomain) => {
+    const updatedDomains = domains.filter(d => d !== domain);
+    setDomains(updatedDomains);
+    localStorage.setItem('domains', JSON.stringify(updatedDomains));
+  };
 
-    // insert new domain to list
-    const addDomain = (domain: IDomain) => {
-        const updatedDomains = [...domains, domain ];
-        setDomains(updatedDomains);
-        localStorage.setItem('domains', JSON.stringify(updatedDomains));
-    };
-
-
-    // delete a saved domain
-    const removeDomain = (domain:IDomain) => {
-        const updatedDomains = domains.filter(d => d !== domain);
-        setDomains(updatedDomains);
-        localStorage.setItem('domains', JSON.stringify(updatedDomains));
-    };
-
-    return {
-        domains,
-        addDomain,
-        removeDomain
-    };
+  return {
+    domains,
+    addDomain,
+    removeDomain
+  };
 };
 
 export default useDomains;
